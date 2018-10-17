a08733c363ca014219a761ddb54d60d98e64c2a1
Rick <39381070+Bickle100101@users.noreply.github.com>
2018-10-17 21:38:09 +0000

    Started on Face DIsplay UI
    About a 3rd of the way to version 1

---
+++ Project Spiral/Assets/_Scripts/Face.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Face", menuName = "Custom/New Face")]
+public class Face : ScriptableObject 
+{
+
+    #region FaceDescription
+    public string faceName;
+    public string job;
+    public Sprite artwork;
+    #endregion
+
+    #region FaceAttributes
+    [Range(-1, 1)]
+    public float agression;
+    [Range(-1, 1)]
+    public float ambition;
+    [Range(-1, 1)]
+    public float integrity;
+    [Range(-1, 1)]
+    public float introspection;
+    [Range(-1, 1)]
+    public float leadership;
+    [Range(-1, 1)]
+    public float luck;
+    public bool isAlive;
+
+    #endregion
+
+}
